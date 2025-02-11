fca46009cd68d182b50f563e04c638c1e9e9955f
pritelabs <sintchia@hotmail.com>
2025-02-11 16:08:10 +0700

    update gitignore

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+import base64
+import os
+import csv
+
+image_folder: str = 'images'
+
+# CSV file containing descriptions
+csv_file = 'descriptions.csv'
+
+# Read description from CSV file
+with open(csv_file, 'r') as file:
+    reader = csv.DictReader(file)
+    descriptions = {row['filename']: row['description'] for row in reader}
+
+# List to hold encoded images
+encoded_images = []
+
+# Encode each image in the folder to base64
+for image_name in os.listdir(image_folder):
+    image_path = os.path.join(image_folder, image_name)
+    with open(image_path, 'rb') as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+        description = descriptions.get(image_name, "No description available")
+        encoded_images.append({
+            "filename": image_name,
+            "description": description,
+            "image": encoded_image
+        })
+
+# Data to be written to JSON file
+data = {
+    "images": encoded_images
+}
+
+# Write data to JSON file
+with open('solution_diagram.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
